cfa55d714535cbd86e81ca4afb32267f895f1992
Otto <otto.laitinen@edu.turkuamk.fi>
2022-02-03 00:23:14 +0200

    added get product offers

--- index.js
+++ index.js
@@ -40,4 +40,18 @@ app.get('/products/:productId/reviews', async (req, res) => {
     }
 })
 
+
+// GET Product OFFERS
+app.get('/products/:productId/offers', async (req, res) => {
+    const { productId } = req.params;
+
+    try {
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/gp/offer-listing/${productId}`);
+
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.json(error);
+    }
+})
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
