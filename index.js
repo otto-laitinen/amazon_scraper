c6ff04202e5ea7d0717d0bff0df5b06fda6b84d9
Otto <otto.laitinen@edu.turkuamk.fi>
2022-02-02 23:50:43 +0200

    added api key, base url, and product details method

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-const express = require('express');         // for creating back-end applications
-const request = require('request-promise'); // for API requests
+const express = require('express');                 // for creating back-end applications
+const request = require('request-promise');         // for API requests
 
-const app = express();                      // initialize application
-const PORT = process.env.PORT  ||  5000;    // use dynamic port or port 5000
+const app = express();                              // initialize application
+const PORT = process.env.PORT  ||  5000;            // use dynamic port or port 5000
 
-app.use(express.json());                    // parse JSON input
+const apiKey = 'b13c5834dd03b7af4cbdae4e490b643e';  // API key
+const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}autoparse=true`
 
-app.get('/', (req, res) => {                // specify what to 'send back' when someone visits this API
+app.use(express.json());                            // parse JSON input
+
+app.get('/', (req, res) => {                        // specify what to 'send back' when someone visits this API
     res.send('Welcome to Amazon Scraper API!');
 });
+
+// GET Product Details
+app.get('/products/:productId', async (req, res) => {
+    const { productId } = req.params;
+
+    try {
+        const response = await request(`${baseUrl}url=https://www.amazon.com/dp/${productId}`);
+
+        res.json(response);
+    } catch (error) {
+        res.json(error);
+    }
+})
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
